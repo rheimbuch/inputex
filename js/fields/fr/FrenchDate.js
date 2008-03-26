160d3cc07c4bdd8149255e5e1c89746267de205e
Maxime Rety <maxime.rety@gmail.com>
2008-03-26 15:14:24 +0000

    Type : "date_fr"

--- js/fields/fr/FrenchDate.js
+++ js/fields/fr/FrenchDate.js
@@ -6,3 +6,8 @@ inputEx.FrenchDate = function(options) {
 	inputEx.FrenchDate.superclass.constructor.call(this,options);
 };
 YAHOO.lang.extend(inputEx.FrenchDate, inputEx.DateField);
+
+/**
+ * Register this class as "date" type
+ */
+inputEx.registerType("date_fr", inputEx.FrenchDate);
