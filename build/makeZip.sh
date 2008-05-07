022bb20cb532fceec32de518efc2ff4b8958384c
Eric Abouaf <eric.abouaf@gmail.com>
2008-05-07 09:55:14 +0000

    Script to generate the project zip file

---
+++ build/makeZip.sh
@@ -0,0 +1,2 @@
+cd ../..
+zip -vr inputex.zip inputex -x "*.svn*" "*.DS_Store*" "*logo-inputex.xcf" "*build.sh" "*makeZip.sh" "*generateDoc.sh" "*inputExCL*" "*inputExDb*" "*inputExRdf*" "*/doc/examples.html" "*_content.html" "*inputex.conf"
