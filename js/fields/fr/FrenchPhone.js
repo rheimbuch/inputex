05e1f3a0873faa0a8063fcac83a0acd40c94cd20
Eric Abouaf <eric.abouaf@gmail.com>
2008-05-07 08:31:05 +0000

    Clean the inputEx repository

--- js/fields/fr/FrenchPhone.js
+++
@@ -1,9 +0,0 @@
-/**
- * @class inputEx.FrenchPhone
- */
-inputEx.FrenchPhone = function(options) {
-	inputEx.FrenchPhone.superclass.constructor.call(this,options);
-	this.options.regexp = /^( *[0-9] *){10}$/;
-	this.options.messages.invalid = "Numéro de téléphone non valide, ex: 06 12 34 56 78";
-};
-YAHOO.lang.extend(inputEx.FrenchPhone, inputEx.Field);
