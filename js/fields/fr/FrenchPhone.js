e49c199e4795d0141b85c6a4a66e1003d7c0964f
Eric Abouaf <eric.abouaf@gmail.com>
2008-07-25 09:31:30 +0000

    FrenchPhone fixed. DatePickerField format example

--- js/fields/fr/FrenchPhone.js
+++ js/fields/fr/FrenchPhone.js
@@ -11,6 +11,6 @@ inputEx.FrenchPhone = function(options) {
 	this.options.regexp = /^( *[0-9] *){10}$/;
 	this.options.messages.invalid = "Numéro de téléphone non valide, ex: 06 12 34 56 78";
 };
-YAHOO.lang.extend(inputEx.FrenchPhone, inputEx.Field);
+YAHOO.lang.extend(inputEx.FrenchPhone, inputEx.StringField);
 
 })();
