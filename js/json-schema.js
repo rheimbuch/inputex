199f81594136d995f676f171ba61821dd0e44b5e
Eric Abouaf <eric.abouaf@gmail.com>
2008-08-19 07:56:30 +0000

    Added more options to the Json Schema implementation

--- js/json-schema.js
+++ js/json-schema.js
@@ -1,67 +1,129 @@
-(function() {
 /**
- * WARNING: the json-schema methods are experimental.
+ * WARNING: the json-schema methods are EXPERIMENTAL.
  *
  * Conversion between inputEx json format and JSON Schema
- * based on "Json Schema Proposal Second Draft" at
+ * based on "Json Schema Proposal Second Draft":
  * http://groups.google.com/group/json-schema/web/json-schema-proposal---second-draft
  *
  * The proposal is still under discussion and the implementation is very minimalist.
  *
+ *
+ * TODO:
+ *    - we should provide a lot of json schema examples and instances that should/should not validate
+ *    - use the $ref (async calls => provide callbacks to methods)
+ *
+ * Limitations:
+ *    - ??? Please do not trust inputEx: the getValue may return a value which do NOT validate the schema (provide an example ?)
+ *    - no tuple typing for arrays
+ *    - no "Union type definition"
+ *
  */
-   var inputEx = YAHOO.inputEx;
+ 
+YAHOO.inputEx.JsonSchema = {
    
    /**
     * Convert a json schema to a inputEx group object config
+    *
     * @param {Object} jsonSchema The evaled JSON schema
     * @param {String} groupName optional, used for the "object" json schema type
     */
-   inputEx.importJsonSchema = function(jsonSchema, groupName) {
-      var ret = { fields: [] };
+   schemaToInputEx: function(p, propertyName) {
       
-      if(groupName) ret.name = groupName;
+      var fieldDef = { label: propertyName, inputParams: { name: propertyName} };
+      if(p.type) {
+         
+         var type = p.type;
+         
+         // If type is a "Union type definition", we'll use the first type for the field
+         // "array" <=>  [] <=> ["any"]
+         if(YAHOO.lang.isArray(type)) {
+            if(type.length == 0 || (type.length == 1 && type[0] == "any") ) {
+               type = "array";
+            }
+            else {
+               type = type[0];
+            }
+         }
+         else if(YAHOO.lang.isObject(type) ) {
+            // What do we do ??
+            console.log("type is an object !!");
+         }
+         
+         fieldDef.type = type;
       
-      for(var key in jsonSchema.properties) {
-         if(jsonSchema.properties.hasOwnProperty(key)) {
-            var p = jsonSchema.properties[key];
-            var field = { label: key, inputParams: { name: key} };
-            if(p.type) {
-               field.type = p.type;
-               
-               if(p.type == "object") {
-                  field.type = "group";
-                  field.inputParams = inputEx.importJsonSchema(p, key);
-               }
-               else if(p.type == "string" && (!!p.options) ) {
-                  field.type = "select";
-                  field.inputParams.selectOptions = [];
-                  field.inputParams.selectValues = [];
-                  for(var i = 0 ; i < p.options.length ; i++) {
-                     var o = p.options[i];
-                     field.inputParams.selectOptions.push(o.label);
-                     field.inputParams.selectValues.push(o.value);
-                  }
-               }
-               else if(p.type == "string") {
-                  if( p.format ) {
-                     if(p.format == "html") {
-                        field.type = "html";
-                     }
-                     else if(p.format == "date") {
-                        field.type = "date";
-                        field.inputParams.tooltipIcon = true;
-                     }
-                  }
+         if(type == "array" ) {
+            fieldDef.type = "list";
+         }
+         else if(type == "object" ) {
+            fieldDef.type = "group";
+            //fieldDef.inputParams = this.schemaToInputEx(p, propertyName);
+            //fieldDef.inputParams = this._parseSchemaProperty(p, propertyName);
+            var groupDef = { fields: [] };
+
+            if(propertyName) groupDef.name = propertyName;
+
+            for(var key in p.properties) {
+               if(p.properties.hasOwnProperty(key)) {
+                  groupDef.fields.push( this.schemaToInputEx(p.properties[key], key) );
                }
             }
-            else if(p["$ref"]) {
-               
+
+            fieldDef.inputParams = groupDef;
+            
+         }
+         else if(type == "string" && (!!p.options) ) {
+            fieldDef.type = "select";
+            fieldDef.inputParams.selectOptions = [];
+            fieldDef.inputParams.selectValues = [];
+            for(var i = 0 ; i < p.options.length ; i++) {
+               var o = p.options[i];
+               fieldDef.inputParams.selectOptions.push(o.label);
+               fieldDef.inputParams.selectValues.push(o.value);
             }
-            ret.fields.push(field);
+         }
+         else if(type == "string") {
+            if( p.format ) {
+               if(p.format == "html") {
+                  fieldDef.type = "html";
+               }
+               else if(p.format == "date") {
+                  fieldDef.type = "date";
+                  fieldDef.inputParams.tooltipIcon = true;
+               }
+            }
          }
       }
+      else if(p["$ref"]){
+         throw new Error("$ref not implemented yet...");
+      }
       
-      return ret;
-   };
+      return fieldDef;
+   },
    
-})();
+   
+   /**
+    * Create an inputEx Json form definition from a json schema instance object
+    * Respect the "Self-Defined Schema Convention"
+    */
+   formFromInstance: function(instanceObject) {
+      
+      if(!instanceObject || !instanceObject["$schema"]) {
+         throw new Error("Invalid json schema instance object. Object must have a '$schema' property.");
+      }
+      
+      var formDef = YAHOO.inputEx.JsonSchema.schemaToInputEx(instanceObject["$schema"]);
+      
+      // Set the default value of each property to the instance value
+      for(var i = 0 ; i < formDef.fields.length ; i++) {
+         var fieldName = formDef.fields[i].inputParams.name;
+         formDef.fields[i].inputParams.value = instanceObject[fieldName];
+      }
+      
+      return formDef;
+   },
+   
+   inputExToSchema: function(inputExJson) {
+      
+   }
+
+};
