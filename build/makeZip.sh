2bc82354a1ec03c5c54bfd995098bcb1f228fce7
Eric Abouaf <eric.abouaf@gmail.com>
2008-11-01 15:39:18 +0000

    removing the yui-compressor from the build zip

--- build/makeZip.sh
+++ build/makeZip.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ../..
-zip -vr inputex-0.2.0.zip inputex-0.2.0 -x "*.svn*" "*.DS_Store*" "*logo-inputex.xcf" "*build.sh" "*makeZip.sh" "*generateDoc.sh" "*inputExRdf*" "*/doc/examples.html" "*_content.html" "*inputex.conf"
+zip -vr inputex-0.2.0.zip inputex-0.2.0 -x "*.svn*" "*.DS_Store*" "*logo-inputex.xcf" "*build.sh" "*makeZip.sh" "*generateDoc.sh" "*inputExRdf*" "*/doc/examples.html" "*_content.html" "*inputex.conf" "*build/tools*"
