ff8e6c7ebe47110fbd44fb3a810ff6c08d08757a
Mingfai Ma <mingfai.ma@gmail.com>
2008-11-24 14:23:26 +0000

    fixed case when there is only one group in an form fields array

--- js/Form.js
+++ js/Form.js
@@ -1,6 +1,6 @@
 (function () {
    var util = YAHOO.util, lang = YAHOO.lang, Event = YAHOO.util.Event, inputEx = YAHOO.inputEx, Dom = util.Dom;
-   
+
 /**
  * @class Create a group of fields within a FORM tag and adds buttons
  * @extends inputEx.Group
@@ -12,13 +12,13 @@
  *   <li>showMask: adds a mask over the form while the request is running (default is false)</li>
  * </ul>
  */
-inputEx.Form = function(options) { 
+inputEx.Form = function(options) {
    inputEx.Form.superclass.constructor.call(this, options);
 };
 
-lang.extend(inputEx.Form, inputEx.Group, 
+lang.extend(inputEx.Form, inputEx.Group,
 /**
- * @scope inputEx.Form.prototype   
+ * @scope inputEx.Form.prototype
  */
 {
 
@@ -28,14 +28,14 @@ lang.extend(inputEx.Form, inputEx.Group,
     */
    setOptions: function(options) {
       inputEx.Form.superclass.setOptions.call(this, options);
-   
+
       this.buttons = [];
-      
+
       this.options.buttons = options.buttons || [];
-      
+
       this.options.action = options.action;
    	this.options.method = options.method;
-   	
+
       if(options.ajax) {
          this.options.ajax = {};
          this.options.ajax.method = options.ajax.method || 'POST';
@@ -45,7 +45,7 @@ lang.extend(inputEx.Form, inputEx.Group,
          this.options.ajax.showMask = lang.isUndefined(options.ajax.showMask) ? false : options.ajax.showMask;
       }
    },
-   
+
     /**
      * Render the group
      */
@@ -108,7 +108,11 @@ lang.extend(inputEx.Form, inputEx.Group,
                 this.form.appendChild(ul);
 
             }else{
-                this.renderFields(this.form);
+                if (this.options.fields.length==1){
+                    this.renderFields(this.form,this.options.fields[0].fields); //if the single element is a group
+                }else{
+                    this.renderFields(this.form,this.options.fields);
+                }
             }
         } else { // a form with a single field
             this.renderFields(this.form);
@@ -120,14 +124,14 @@ lang.extend(inputEx.Form, inputEx.Group,
             this.disable();
         }
     },
-   
+
    /**
-    * Render the buttons 
+    * Render the buttons
     */
    renderButtons: function() {
-      
+
       this.buttonDiv = inputEx.cn('div', {className: 'inputEx-Form-buttonBar'});
-		
+
 	   var button, buttonEl;
 	   for(var i = 0 ; i < this.options.buttons.length ; i++ ) {
 	      button = this.options.buttons[i];
@@ -135,12 +139,12 @@ lang.extend(inputEx.Form, inputEx.Group,
 	      if( button.onClick ) { buttonEl.onclick = button.onClick; }
 	      this.buttons.push(buttonEl);
 	      this.buttonDiv.appendChild(buttonEl);
-	   }	
-	   
+	   }
+
 	   this.form.appendChild(this.buttonDiv);
    },
-   
-   
+
+
    /**
     * Init the events
     */
@@ -150,7 +154,7 @@ lang.extend(inputEx.Form, inputEx.Group,
       // Handle the submit event
       Event.addListener(this.form, 'submit', this.options.onSubmit || this.onSubmit,this,true);
    },
-   
+
    /**
     * Intercept the 'onsubmit' event and stop it if !validate
     * If the ajax option object is set, use YUI async Request to send the form
@@ -159,56 +163,56 @@ lang.extend(inputEx.Form, inputEx.Group,
    onSubmit: function(e) {
 	   if ( !this.validate() ) {
 		   Event.stopEvent(e);
-	   } 
+	   }
 	   if(this.options.ajax) {
 		   Event.stopEvent(e);
 	      this.asyncRequest();
 	   }
    },
-  
+
    /**
     * Send the form value in JSON through an ajax request
     */
-   asyncRequest: function() { 
-      
+   asyncRequest: function() {
+
       if(this.options.ajax.showMask) { this.showMask(); }
 	   var postData = "value="+lang.JSON.stringify(this.getValue());
-      util.Connect.asyncRequest(this.options.ajax.method, this.options.ajax.uri, { 
+      util.Connect.asyncRequest(this.options.ajax.method, this.options.ajax.uri, {
          success: function(o) {
             if(this.options.ajax.showMask) { this.hideMask(); }
             if( lang.isFunction(this.options.ajax.callback.success) ) {
                this.options.ajax.callback.success.call(this.options.ajax.callback.scope,o);
             }
-         }, 
-      
+         },
+
          failure: function(o) {
             if(this.options.ajax.showMask) { this.hideMask(); }
             if( lang.isFunction(this.options.ajax.callback.failure) ) {
                this.options.ajax.callback.failure.call(this.options.ajax.callback.scope,o);
             }
-         }, 
-      
-         scope:this 
+         },
+
+         scope:this
       }, postData);
    },
-  
+
    /**
     * Create a Mask over the form
     */
    renderMask: function() {
       if(this.maskRendered) return;
-      
+
       // position as "relative" to position formMask inside as "absolute"
       Dom.setStyle(this.divEl, "position", "relative");
-      
+
       // set zoom = 1 to fix hasLayout issue with IE6/7
       if (YAHOO.env.ua.ie) { Dom.setStyle(this.divEl, "zoom", 1); }
-      
+
       // Render mask over the divEl
       this.formMask = inputEx.cn('div', {className: 'inputEx-Form-Mask'},
          {
-            display: 'none', 
-            // Use offsetWidth instead of Dom.getStyle(this.divEl,"width") because 
+            display: 'none',
+            // Use offsetWidth instead of Dom.getStyle(this.divEl,"width") because
             // would return "auto" with IE instead of size in px
             width: this.divEl.offsetWidth+"px",
             height: this.divEl.offsetHeight+"px"
@@ -223,10 +227,10 @@ lang.extend(inputEx.Form, inputEx.Group,
     */
    showMask: function() {
       this.renderMask();
-      
+
       // Hide selects in IE 6
       this.toggleSelectsInIE(false);
-      
+
       this.formMask.style.display = '';
    },
 
@@ -234,13 +238,13 @@ lang.extend(inputEx.Form, inputEx.Group,
     * Hide the form mask
     */
    hideMask: function() {
-      
+
       // Show selects back in IE 6
       this.toggleSelectsInIE(true);
 
       this.formMask.style.display = 'none';
    },
-   
+
    /*
    * Method to hide selects in IE 6 when masking the form (else they would appear over the mask)
    */
@@ -257,8 +261,8 @@ lang.extend(inputEx.Form, inputEx.Group,
          );
       }
    },
-   
-   
+
+
    /**
     * Enable all fields and buttons in the form
     */
@@ -268,7 +272,7 @@ lang.extend(inputEx.Form, inputEx.Group,
  	      this.buttons[i].disabled = false;
       }
    },
-   
+
    /**
     * Disable all fields and buttons in the form
     */
@@ -282,7 +286,7 @@ lang.extend(inputEx.Form, inputEx.Group,
 });
 
 
-// Specific waiting message in ajax submit 
+// Specific waiting message in ajax submit
 inputEx.messages.ajaxWait = "Please wait...";;
 
 /**
