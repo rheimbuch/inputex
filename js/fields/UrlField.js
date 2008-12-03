af2adda49b1b1199ca7e670f375fcd9441a5da68
Mingfai Ma <mingfai.ma@gmail.com>
2008-12-03 03:53:03 +0000

    inputEx-107

--- js/fields/UrlField.js
+++ js/fields/UrlField.js
@@ -15,10 +15,10 @@ inputEx.UrlField = function(options) {
    inputEx.UrlField.superclass.constructor.call(this,options);
 };
 
-lang.extend(inputEx.UrlField, inputEx.StringField, 
+lang.extend(inputEx.UrlField, inputEx.StringField,
 /**
- * @scope inputEx.UrlField.prototype   
- */   
+ * @scope inputEx.UrlField.prototype
+ */
 {
 
    /**
@@ -27,56 +27,56 @@ lang.extend(inputEx.UrlField, inputEx.StringField,
     */
    setOptions: function(options) {
       inputEx.UrlField.superclass.setOptions.call(this, options);
-      
+
       this.options.className = options.className ? options.className : "inputEx-Field inputEx-UrlField";
       this.options.messages.invalid = inputEx.messages.invalidUrl;
       this.options.favicon = lang.isUndefined(options.favicon) ? (("https:" == document.location.protocol) ? false : true) : options.favicon;
-      
+
       // validate with url regexp
       this.options.regexp = inputEx.regexps.url;
    },
-   
+
    /**
     * Adds a img tag before the field to display the favicon
     */
    render: function() {
       inputEx.UrlField.superclass.render.call(this);
-      this.el.size = 27;
-      
+      this.el.size = 50;
+
       if(!this.options.favicon) {
          YAHOO.util.Dom.addClass(this.el, 'nofavicon');
       }
 
       // Create the favicon image tag
       if(this.options.favicon) {
-         this.favicon = inputEx.cn('img', {src: inputEx.spacerUrl});         
+         this.favicon = inputEx.cn('img', {src: inputEx.spacerUrl});
          this.fieldContainer.insertBefore(this.favicon,this.fieldContainer.childNodes[0]);
-         
+
          // focus field when clicking on favicon
          YAHOO.util.Event.addListener(this.favicon,"click",function(){this.focus();},this,true);
       }
    },
-   
+
    setClassFromState: function() {
       inputEx.UrlField.superclass.setClassFromState.call(this);
-      
+
       if(this.options.favicon) {
          // try to update with url only if valid url (else pass null to display inputEx.spacerUrl)
          this.updateFavicon((this.previousState == inputEx.stateValid) ? this.getValue() : null);
       }
    },
-   
+
 
    updateFavicon: function(url) {
-      var newSrc = url ? (url+"/favicon.ico") : inputEx.spacerUrl;
+      var newSrc = url ? url.match(/https?:\/\/[^\/]*/)+'/favicon.ico' : inputEx.spacerUrl;
       if(newSrc != this.favicon.src) {
-      
+
          // Hide the favicon
          inputEx.sn(this.favicon, null, {visibility: 'hidden'});
-   
+
          // Change the src
          this.favicon.src = newSrc;
-   
+
          // Set the timer to launch displayFavicon in 1s
          if(this.timer) { clearTimeout(this.timer); }
          var that = this;
