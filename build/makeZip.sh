c99a0d81c35b00ca451d92d62c0b28e03cee44e8
Eric Abouaf <eric.abouaf@gmail.com>
2009-01-22 21:08:07 +0100

    Zipping script

--- build/makeZip.sh
+++ build/makeZip.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd ../..
-zip -vr inputex-0.2.1.zip inputex-0.2.1 -x "*.git*" "*.DS_Store*" "*logo-inputex.xcf" "*build.sh" "*makeZip.sh" "*generateDoc.sh" "*inputExRdf*" "*/doc/examples.html" "*_content.html" "*inputex.conf" "*build/tools*"
+zip -vr inputex-0.2.1.zip inputex-0.2.1 -x "*.zip" "*.git*" "*.DS_Store*" "*logo-inputex.xcf" "*build.sh" "*makeZip.sh" "*generateDoc.sh" "*inputExRdf*" "*/doc/examples.html" "*_content.html" "*inputex.conf" "*build/tools*"
